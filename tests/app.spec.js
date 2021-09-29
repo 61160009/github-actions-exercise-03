5d896d2b423fd5d47d995105ae6f4adedc8ddc0c
Thaprakorn Yamthip <61160009@go.buu.ac.th>
2021-09-29 21:07:23 +0700

    github-exercise-test

---
+++ tests/app.spec.js
@@ -0,0 +1,13 @@
+const request = require('supertest')
+const app = require('../app')
+
+describe('GET /', () => {
+    it('responds with "Hello world!"', (done) => {
+        request(app).get('/').expect('Hello world!', done)
+    })
+})
+describe('GET /users', () => {
+    it('responds with "No user in the system"', (done) => {
+        request(app).get('/users').expect('No user in the system', done)
+    })
+})
